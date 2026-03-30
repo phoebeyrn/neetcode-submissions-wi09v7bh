613eda932739cc88495ec5d71f1590edb005743c
Phoebe Wu <phoebemeiwu@gmail.com>
2026-03-29 22:16:03 -0700

    Add: eating-bananas - submission-10

---
+++ Data Structures & Algorithms/eating-bananas/submission-10.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) 
+    {
+        std::vector<int>::iterator max_it = std::max_element(piles.begin(), piles.end());
+        int speed = *max_it;
+
+        int l = 1;
+        int r = *max_it - 1;
+
+        while(l <= r)
+        {
+            int m = l + (r - l) / 2;
+            long long hours = 0;
+            for(int i = 0; i < piles.size(); i++)
+            {
+                hours += static_cast<double>(ceil((double)piles[i] / m));
+            }
+
+            if(hours <= h)
+            {
+                speed = min(m, speed);
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+
+        }
+
+        return speed;
+    }
+};
