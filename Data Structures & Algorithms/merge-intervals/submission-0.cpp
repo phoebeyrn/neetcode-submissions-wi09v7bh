29ff7152894da3fc07ea184b3351418b29751c1b
Phoebe Wu <phoebemeiwu@gmail.com>
2026-04-11 23:06:21 -0700

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for(const auto& interval : intervals)
+        {
+            int start = interval[0];
+            int end = interval[1];
+
+            int lastEnd = res.back()[1];
+
+            if(start <= lastEnd)
+            {
+                res.back()[1] = max(lastEnd, end);
+            }
+            else
+            {
+                res.push_back({start, end});
+            }
+        }
+
+        return res;
+    }
+};
