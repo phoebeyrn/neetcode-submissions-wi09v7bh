4ce7c9d3fec25909146804c4795a20516dbb2156
Phoebe Wu <phoebemeiwu@gmail.com>
2026-04-12 22:42:12 -0700

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) 
+    {
+        int a = 1, b = 1;
+        for(int i = 0; i < n; i++)
+        {
+            int temp = a;
+            a += b;
+            b = temp;
+        }
+
+        return b;
+    }
+};
