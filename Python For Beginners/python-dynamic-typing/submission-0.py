5d576eba81d76a499ee9829d91792986d9e4837e
Phoebe Wu <phoebemeiwu@gmail.com>
2026-08-09 15:21:19 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 0
+print(type(variable))
+variable = 0.9
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "lol"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
